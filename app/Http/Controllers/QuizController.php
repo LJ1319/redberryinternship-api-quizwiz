00ca4803db6e30ea099115380424b2acff6330b6
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-04-22 01:12:32 +0400

    feat: implement filtering

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -8,6 +8,20 @@ class QuizController extends Controller
 {
 	public function index()
 	{
-		return Quiz::filter(request(['search', 'categories']))->with(['users', 'level', 'categories', 'questions', 'questions.answers'])->paginate(9);
+		$orderColumn = request('order_column', 'created_at');
+		if (!in_array($orderColumn, ['name', 'created_at', 'users_count'])) {
+			$orderColumn = 'created_at';
+		}
+
+		$orderDirection = request('order_direction', 'desc');
+		if (!in_array($orderDirection, ['asc', 'desc'])) {
+			$orderDirection = 'desc';
+		}
+
+		return Quiz::filter(request(['search', 'filter', 'levels', 'categories']))
+					->with(['users', 'level', 'categories', 'questions', 'questions.answers'])
+					->withCount('users')
+					->orderBy($orderColumn, $orderDirection)
+					->paginate(9);
 	}
 }
